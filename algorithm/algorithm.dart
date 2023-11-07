c075a03287b26780e8bcc860bbf520628a05caa2
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2023-11-07 23:37:07 +0700

    update algorithm
    add bonus function countTotal, findMin, findMax, findEven, findOdd

--- algorithm/algorithm.dart
+++ algorithm/algorithm.dart
@@ -7,7 +7,40 @@ void miniMaxSum(List<int> arr) {
   print('$minSum $maxSum');
 }
 
+void countTotal(List<int> arr) {
+  int total = arr.length;
+  print('Count: $total');
+}
+
+void findMin(List<int> arr) {
+  int min = arr.reduce((a, b) => a < b ? a : b);
+  print('Min value: $min');
+}
+
+void findMax(List<int> arr) {
+  int max = arr.reduce((a, b) => a > b ? a : b);
+  print('Max value: $max');
+}
+
+void findEven(List<int> arr) {
+  List<int> evenElements = arr.where((element) => element % 2 == 0).toList();
+  print('Even value in the array: $evenElements');
+}
+
+void findOdd(List<int> arr) {
+  List<int> oddElements = arr.where((element) => element % 2 != 0).toList();
+  print('Odd value in the array: $oddElements');
+}
+
 void main() {
   List<int> arr = [1, 2, 3, 4, 5]; // Replace with your input list
+
   miniMaxSum(arr);
+
+  // Bonus
+  countTotal(arr);
+  findMin(arr);
+  findMax(arr);
+  findEven(arr);
+  findOdd(arr);
 }
